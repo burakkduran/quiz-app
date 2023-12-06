3da8fb7709255283efafd9191e18124542340ca5
Burak Duran <burak.duraninfo@gmail.com>
2023-12-06 06:22:07 +0300

    fix: QuizCard css and add category

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -22,7 +22,6 @@ const QuestionCard = ({
     if (count === amount - 1) {
       setIsAnswered(true);
     }
-    
   };
 
   useEffect(() => {
@@ -38,11 +37,18 @@ const QuestionCard = ({
   }, [timer, count, setcount]);
 
   return (
-    <div className="mb-32 flex flex-col items-center">
-      <div className="bg-violet-950 w-12 h-12 rounded-full text-white text-lg flex items-center justify-center place-self-start ">
-        {timer}
+    <div className="container flex flex-col items-center justify-center ">
+      <div className="w-3/4 flex items-center gap-4 mb-6">
+        <div className="bg-violet-950 w-12 h-12 rounded-full text-white text-lg flex items-center justify-center flex-shrink-0">
+          {timer}
+        </div>
+        <div className="text-white text-2xl">
+          <span className="text-slate-900">Category:</span>{" "}
+          {questionData[count]?.category}
+        </div>
       </div>
-      <div className="flex items-center text-2xl gap-4 border-b border-r pr-3 pb-3 rounded-br-xl border-violet-500 mb-12 max-w-4xl w-3/4 sm:w-full">
+
+      <div className="flex items-center text-2xl gap-4 border-b border-t border-r pr-3 px-3 rounded-br-xl rounded-tr-xl border-l-2 border-l-black border-violet-500 mb-12 w-3/4 h-full sm:h-24 md:h-16">
         <div className="w-12 h-12 rounded-full text-black text-2xl flex items-center justify-center flex-shrink-0">
           {count + 1}/{amount}
         </div>
@@ -53,7 +59,7 @@ const QuestionCard = ({
       <div className="flex gap-6 flex-wrap justify-center items-center max-w-2xl">
         {questionData[count]?.answers.map((answer, i) => (
           <button
-            className="bg-violet-800 text-xl hover:bg-violet-200 hover:text-violet-800 transition duration-200 hover:border hover:border-violet-800 text-white w-64 h-12 rounded-xl "
+            className="bg-violet-800 text-xl hover:bg-violet-200 hover:text-violet-800 transition duration-200 hover:border hover:border-violet-800 text-white w-64 py-2 border border-transparent rounded-xl "
             key={i}
             value={answer}
             onClick={approvedChoice}
